81d7a493f3d832caa0e344c28a0a9501e2e50729
Ganesh Kondal <ganeshkondal@gmail.com>
2013-08-12 22:50:21 +0530

    css and node server js file

--- js/angularControllers.js
+++ js/angularControllers.js
@@ -1,12 +1,12 @@
 // no dependencies for this controller
-var demoApp = angular.module('demoApp', ['ngSanitize']);
+var demoApp = angular.module('demoApp', ['ngSanitize', 'ngResource']);
 
 // Routers
 // Gluing View and the controller
 // instead of having demoApp - in all thes.me places,
 // the previous semi-colon could be taken and - config, factory can be
 // with a dot '.' beginning - this is called chaining
-demoApp.config(function($routeProvider) {
+demoApp.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/books', {
             controller: 'FindBookController',
@@ -21,47 +21,10 @@ demoApp.config(function($routeProvider) {
             redirectTo: '/books'
         });
 
+        $locationProvider.html5Mode( true );
+
 });
 
-// demoApp.factory('booksFactory', function() {
-//     var factory = {};
-//     var books = [{
-//         title: 'Angular JS',
-//         price : '14.48',
-//         author: 'Brad Green and Shyam Seshadri',
-//         publicationDate: 'Dec 21,2012',            
-//         imgUrl: 'AngularJS.jpg'
-//     }, {
-//         title: 'Developing an AngularJS Edge',
-//         price : '19.95',
-//         author: 'Christoper Hiller and Troy Mott',
-//         publicationDate: 'Dec 20,2012',
-//         imgUrl: 'DevolopingAnAngularEdge.jpg'
-//     }, {
-//         title: 'Recipes with Angular JS',
-//         price : '9.99',
-//         author: 'Fredrick Deitz',
-//         publicationDate: 'Dec 20,2012',
-//         imgUrl: 'RecipesWithAngularJS.jpg'        
-
-//     },
-//     {
-//         title: 'Responsive Web Design',
-//         price : '19.29',
-//         author: 'Tim Kadlec and Aaron Gustafson',
-//         publicationDate: 'Dec 20,2012',
-//         imgUrl: 'ResponsiveWebDesign.jpg'        
-//     }
-//     ];
-
-//     // Factory Method that fetches the list of books.
-//     // Either from a backend service or via cached dataset.
-//     factory.getBooks = function() {
-//         return books;
-//     };
-//     return factory;
-
-// });
 
 // sample factory being passed here;
 // $scope, factory variable passed via dependency injection
