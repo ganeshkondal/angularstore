f7d359db0040ae3173b75a77121e1f4cd600d822
Ganesh Kondal <ganeshkondal@gmail.com>
2013-08-12 18:08:07 +0530

    Movign on from tomocat to Node Server

--- js/angularControllers.js
+++ js/angularControllers.js
@@ -23,45 +23,45 @@ demoApp.config(function($routeProvider) {
 
 });
 
-demoApp.factory('booksFactory', function() {
-    var factory = {};
-    var books = [{
-        title: 'Angular JS',
-        price : '$14.48',
-        author: 'Brad Green and Shyam Seshadri',
-        publicationDate: 'Dec 20,2012',            
-        imgUrl: 'AngularJS.jpg'
-    }, {
-        title: 'Developing an AngularJS Edge',
-        price : '$19.95',
-        author: 'Christoper Hiller and Troy Mott',
-        publicationDate: 'Dec 20,2012',
-        imgUrl: 'DevolopingAnAngularEdge.jpg'
-    }, {
-        title: 'Recipes with Angular JS',
-        price : '$9.99',
-        author: 'Fredrick Deitz',
-        publicationDate: 'Dec 20,2012',
-        imgUrl: 'RecipesWithAngularJS.jpg'        
+// demoApp.factory('booksFactory', function() {
+//     var factory = {};
+//     var books = [{
+//         title: 'Angular JS',
+//         price : '14.48',
+//         author: 'Brad Green and Shyam Seshadri',
+//         publicationDate: 'Dec 21,2012',            
+//         imgUrl: 'AngularJS.jpg'
+//     }, {
+//         title: 'Developing an AngularJS Edge',
+//         price : '19.95',
+//         author: 'Christoper Hiller and Troy Mott',
+//         publicationDate: 'Dec 20,2012',
+//         imgUrl: 'DevolopingAnAngularEdge.jpg'
+//     }, {
+//         title: 'Recipes with Angular JS',
+//         price : '9.99',
+//         author: 'Fredrick Deitz',
+//         publicationDate: 'Dec 20,2012',
+//         imgUrl: 'RecipesWithAngularJS.jpg'        
 
-    },
-    {
-        title: 'Responsive Web Design',
-        price : '$19.29',
-        author: 'Tim Kadlec and Aaron Gustafson',
-        publicationDate: 'Dec 20,2012',
-        imgUrl: 'ResponsiveWebDesign.jpg'        
-    }
-    ];
+//     },
+//     {
+//         title: 'Responsive Web Design',
+//         price : '19.29',
+//         author: 'Tim Kadlec and Aaron Gustafson',
+//         publicationDate: 'Dec 20,2012',
+//         imgUrl: 'ResponsiveWebDesign.jpg'        
+//     }
+//     ];
 
-    // Factory Method that fetches the list of books.
-    // Either from a backend service or via cached dataset.
-    factory.getBooks = function() {
-        return books;
-    };
-    return factory;
+//     // Factory Method that fetches the list of books.
+//     // Either from a backend service or via cached dataset.
+//     factory.getBooks = function() {
+//         return books;
+//     };
+//     return factory;
 
-});
+// });
 
 // sample factory being passed here;
 // $scope, factory variable passed via dependency injection
@@ -78,7 +78,7 @@ demoApp.controller('FindBookController', function($scope, $location, booksFactor
     }
 
 
-    $scope.addBook = function( $location ) {
+    $scope.addBook = function() {
         $scope.newBook.hidePreview=false;
         $scope.books.push({
             price: $scope.newBook.price,
